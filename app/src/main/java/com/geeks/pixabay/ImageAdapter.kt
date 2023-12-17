c93550a25bd781c146cb406a75ad30cfc3459912
Daniyar Bolotbekov <trigoon7@gmail.com>
2023-12-17 20:36:05 +0600

    updated commit

--- app/src/main/java/com/geeks/pixabay/ImageAdapter.kt
+++ app/src/main/java/com/geeks/pixabay/ImageAdapter.kt
@@ -15,7 +15,7 @@ class ImageAdapter(var list: ArrayList<ImageModel>) : Adapter<ImageAdapter.Image
         }
     }
 
-    //okoko
+    //lalala2
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder =
         ImageViewHolder(
